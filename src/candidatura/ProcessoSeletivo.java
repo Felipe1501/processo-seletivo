4cbc8932c4aa5edca4b80bac83027cbb11854e83
Felipe1501 <felipebsantos1501@gmail.com>
2023-08-24 13:18:44 -0300

    imprimindo selecionado

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -5,7 +5,20 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("PROCESSO SELETIVO");
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE", "YURI ALBERTO", "RENATO AUGUSTO", "GIL"};
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+        for(int indice=0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de nº" + (indice + 1) + " é o " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação foreach");
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos(){
